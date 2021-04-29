9cfa62fcb1f7895ae839ac679860cf6cfa7b88d1
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-04-30 04:13:11 +0500

    Update __main__.py

--- LaylaRobot/__main__.py
+++ LaylaRobot/__main__.py
@@ -78,7 +78,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/f9631797f7fec76c886c7.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/ff917329746ab63e5ce1d.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Supporting Team >> [ＧＥＮＥＲＡＬツ★](t.me/Animeweebs2)"""
