b9b4d47e95bf4ddefffa5d7ee989943c141b5934
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-01 05:23:28 +0500

    Update and rename LaylaRobot/__main__.py to ElizaRobot/__main__.py

--- ElizaRobot/__main__.py
+++ ElizaRobot/__main__.py
@@ -5,14 +5,14 @@ import re
 from sys import argv
 from typing import Optional
 
-from LaylaRobot import (ALLOW_EXCL, CERT_PATH, DONATION_LINK, LOGGER,
+from ElizaRobot import (ALLOW_EXCL, CERT_PATH, DONATION_LINK, LOGGER,
                           OWNER_ID, PORT, SUPPORT_CHAT, TOKEN, URL, WEBHOOK,
                           dispatcher, StartTime, telethn, updater)
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
-from LaylaRobot.modules import ALL_MODULES
-from LaylaRobot.modules.helper_funcs.chat_status import is_user_admin
-from LaylaRobot.modules.helper_funcs.misc import paginate_modules
+from ElizaRobot.modules import ALL_MODULES
+from ElizaRobot.modules.helper_funcs.chat_status import is_user_admin
+from ElizaRobot.modules.helper_funcs.misc import paginate_modules
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.error import (BadRequest, ChatMigrated, NetworkError,
