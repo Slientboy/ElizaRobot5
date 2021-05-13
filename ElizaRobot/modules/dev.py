9627b3c6fb11851b969c8e43ee3aab72f78856dc
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-14 01:18:00 +0500

    Update and rename LaylaRobot/modules/dev.py to ElizaRobot/modules/dev.py

--- ElizaRobot/modules/dev.py
+++ ElizaRobot/modules/dev.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 from time import sleep
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus
+from ElizaRobot import dispatcher
+from ElizaRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
