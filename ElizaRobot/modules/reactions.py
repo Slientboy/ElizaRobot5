81c945f9600b73c297ce4d950e36a4da7d8190e6
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-14 11:26:03 +0500

    Update and rename LaylaRobot/modules/reactions.py to ElizaRobot/modules/reactions.py

--- ElizaRobot/modules/reactions.py
+++ ElizaRobot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from ElizaRobot import dispatcher
+from ElizaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
