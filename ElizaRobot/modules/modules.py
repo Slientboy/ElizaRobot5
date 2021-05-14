d2349ccdf2bd5c9871cbf3107ad948170e3a0dbd
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-14 10:40:17 +0500

    Update and rename LaylaRobot/modules/modules.py to ElizaRobot/modules/modules.py

--- ElizaRobot/modules/modules.py
+++ ElizaRobot/modules/modules.py
@@ -1,10 +1,10 @@
 import importlib
 
-from LaylaRobot import dispatcher
-from LaylaRobot.__main__ import (CHAT_SETTINGS, DATA_EXPORT, DATA_IMPORT,
+from ElizaRobot import dispatcher
+from ElizaRobot.__main__ import (CHAT_SETTINGS, DATA_EXPORT, DATA_IMPORT,
                                    HELPABLE, IMPORTED, MIGRATEABLE, STATS,
                                    USER_INFO, USER_SETTINGS)
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from ElizaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
