fae15eb2ca30919e5bb987e8f1dd4c491557a85e
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-14 13:26:35 +0500

    Update and rename LaylaRobot/modules/sql/cust_filters_sql.py to ElizaRobot/modules/sql/cust_filters_sql.py

--- ElizaRobot/modules/sql/cust_filters_sql.py
+++ ElizaRobot/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from LaylaRobot.modules.helper_funcs.msg_types import Types
-from LaylaRobot.modules.sql import BASE, SESSION
+from ElizaRobot.modules.helper_funcs.msg_types import Types
+from ElizaRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
