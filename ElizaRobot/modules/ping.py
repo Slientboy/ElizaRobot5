630e01f3558e25d968d83b4370bea3525310acbb
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-14 11:20:12 +0500

    Update and rename LaylaRobot/modules/ping.py to ElizaRobot/modules/ping.py

--- ElizaRobot/modules/ping.py
+++ ElizaRobot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from LaylaRobot import StartTime, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import sudo_plus
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from ElizaRobot import StartTime, dispatcher
+from ElizaRobot.modules.helper_funcs.chat_status import sudo_plus
+from ElizaRobot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
